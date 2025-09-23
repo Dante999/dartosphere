b013f246bdeacac0d4474e0ceee949107067e78c
Dante999 <dante999@users.noreply.github.com>
2025-09-23 20:55:11 +0200

    Add unittests for game_x01 checkout master

--- tests/test_game_x01.c
+++ tests/test_game_x01.c
@@ -181,7 +181,7 @@ static void test_remaining_points_when_checkout_double(void)
 			.checkout_mode   = X01_MODE_DOUBLE,
 			.player_score    = 10,
 			.current_throw   = dart_hit_make(DARTS_SINGLE, 9),
-			.expected_result = X01_RESULT_PLAYER_OVERSHOOT 
+			.expected_result = X01_RESULT_PLAYER_OVERSHOOT
 		},
 		{
 			.testname = "GIVEN_checkout_double_WHEN_one_point_left_WITH_double_throw_THEN_player_overshoot",
@@ -244,9 +244,81 @@ static void test_remaining_points_when_checkout_double(void)
 	run_testcases(cases, ARRAY_SIZE(cases));
 }
 
+static void test_remaining_points_when_checkout_master(void)
+{
+	struct Checkout_Testcases cases[] = {
+		{
+			.testname = "GIVEN_checkout_master_WHEN_one_point_left_WITH_single_throw_THEN_player_overshoot",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 10,
+			.current_throw   = dart_hit_make(DARTS_SINGLE, 9),
+			.expected_result = X01_RESULT_PLAYER_OVERSHOOT
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_one_point_left_WITH_double_throw_THEN_player_overshoot",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 9,
+			.current_throw   = dart_hit_make(DARTS_DOUBLE, 4),
+			.expected_result = X01_RESULT_PLAYER_OVERSHOOT
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_one_point_left_WITH_tripple_throw_THEN_player_overshoot",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 10,
+			.current_throw   = dart_hit_make(DARTS_TRIPPLE, 3),
+			.expected_result = X01_RESULT_PLAYER_OVERSHOOT
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_two_points_left_WITH_single_throw_THEN_player_can_continue",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 10,
+			.current_throw   = dart_hit_make(DARTS_SINGLE, 8),
+			.expected_result = X01_RESULT_CONTINUE
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_two_points_left_WITH_double_throw_THEN_player_can_continue",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 10,
+			.current_throw   = dart_hit_make(DARTS_DOUBLE, 4),
+			.expected_result = X01_RESULT_CONTINUE
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_two_points_left_WITH_tripple_throw_THEN_player_can_continue",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 11,
+			.current_throw   = dart_hit_make(DARTS_TRIPPLE, 3),
+			.expected_result = X01_RESULT_CONTINUE
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_three_points_left_WITH_single_throw_THEN_player_can_continue",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 10,
+			.current_throw   = dart_hit_make(DARTS_SINGLE, 7),
+			.expected_result = X01_RESULT_CONTINUE
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_three_points_left_WITH_double_throw_THEN_player_can_continue",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 11,
+			.current_throw   = dart_hit_make(DARTS_DOUBLE, 4),
+			.expected_result = X01_RESULT_CONTINUE
+		},
+		{
+			.testname = "GIVEN_checkout_master_WHEN_three_points_left_WITH_tripple_throw_THEN_player_can_continue",
+			.checkout_mode   = X01_MODE_MASTER,
+			.player_score    = 12,
+			.current_throw   = dart_hit_make(DARTS_TRIPPLE, 3),
+			.expected_result = X01_RESULT_CONTINUE
+		},
+	};
+
+	run_testcases(cases, ARRAY_SIZE(cases));
+}
+
 void main_test_game_x01(void)
 {
 	RUNTEST(test_finisher);
 	RUNTEST(test_remaining_points_when_checkout_straight);
 	RUNTEST(test_remaining_points_when_checkout_double);
+	RUNTEST(test_remaining_points_when_checkout_master);
 }
